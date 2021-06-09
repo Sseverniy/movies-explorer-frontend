08d120334c45f74ee1f3e696d4835a053554b77f
Aygul <goodblessyo@yandex.ru>
2021-06-09 14:22:08 +0300

    feat: сверстать шапку страницы о проекте

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import './Header.css';
+import logo from '../../images/logo.svg'
+
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" alt="Логотип сайта" src={logo}></img>
+      <div className="header__wrapper">
+        <button className="header__register-button button">Регистрация</button>
+        <button className="header__login-button button">Войти</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
