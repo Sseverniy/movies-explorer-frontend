b37d79fae33b08cc959d239b44f21e0f4b7b2c78
Aygul <goodblessyo@yandex.ru>
2021-06-25 23:08:13 +0300

    feat: добавить кнопку удаления фильмов

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Header.css';
 import Navigation from '../Navigation/Navigation';
-import logo from '../../images/logo.svg';
 import {useLocation, Link} from "react-router-dom";
 
 function Header({loggedIn}) {
@@ -11,7 +10,6 @@ function Header({loggedIn}) {
     (location.pathname === "/signin" || location.pathname === "/signout" || location.pathname === "/notFound") ?
     '' :
     <header className="header">
-      {/* <img className="header__logo" alt="Логотип сайта" src={logo}/> */}
       <Link exact to="/" className="header__logo"/>
       <Navigation loggedIn={loggedIn}/>
     </header>
--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import "./MoviesCard.css";
 import moviePreview from "../../images/movie-preview.png";
+import {useLocation} from "react-router-dom";
 
 function MoviesCard({savedMovie, onSave}) {
+  const location = useLocation();
   return (
+    location.pathname === "/saved-movies" ?
     <li className="movie">
       <img className="movie__preview" alt="Превью фильма" src={moviePreview}/>
       <div className="movie__wrapper">
         <h2 className="movie__title">33 слова о дизайне</h2>
-        <button className={savedMovie ? "movie__save-button movie__save-button_active":"movie__save-button" } type="button" aria-label="Сохранить фильм" onClick={onSave}/>
+        <button className="movie__delete-button" type="button" aria-label="Удалить фильм" />
       </div>
       <p className="movie__duration">1ч42м</p>
     </li>
-
+    :
+    <li className="movie">
+    <img className="movie__preview" alt="Превью фильма" src={moviePreview}/>
+    <div className="movie__wrapper">
+      <h2 className="movie__title">33 слова о дизайне</h2>
+      <button className={savedMovie ? "movie__save-button movie__save-button_active" : "movie__save-button" } type="button" aria-label="Сохранить фильм" onClick={onSave}/>
+    </div>
+    <p className="movie__duration">1ч42м</p>
+  </li>
   )
 }
 
