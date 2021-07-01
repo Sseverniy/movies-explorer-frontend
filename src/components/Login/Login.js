425229de6aa593dff975d872afce5a8088c9b131
Aygul <goodblessyo@yandex.ru>
2021-07-01 14:58:45 +0300

    feat: добавить регистрацию и авторизацию

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,31 +1,49 @@
 import React from 'react';
 import "./Login.css";
 import SignHeader from "../SignHeader/SignHeader";
+import * as MainApi from "../../utils/MainApi";
+import { useFormHook } from "../../utils/useFormHook";
 import {Link, useHistory} from "react-router-dom";
 
 function Login({onLogin}) {
   const history = useHistory();
-  function submitLogin() {
-    onLogin();
-    history.push("/movies");
+  const { values, handleChange, errors, isValid } = useFormHook();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    MainApi.login(values)
+      .then((res)=>{
+        if(res.statusCode !== 400){
+          console.log('Вы успешно вошли в профиль!');
+          onLogin();
+          history.push('/movies');
+        } else {
+          console.log('Что-то пошло не так! Попробуйте ещё раз.');
+        }
+      })
+      .catch((err)=> {
+        console.log(err);
+      })
   }
   return (
-    <div className="sign-container">
+    <div className="login-container">
       <SignHeader 
         headerText="Рады видеть!"
       />
-      <form className="sign-form">
-        <label for="user-email" className="sign-form__label">E-mail
-          <input id="user-email" className="sign-form__input" type="email" required></input>
+      <form className="login-form">
+        <label for="user-email" className={`login-form__label ${errors.email ? 'login-form__label_error': ''}`}>E-mail
+          <input onChange={handleChange} value={values.email} name='email' id="user-email" className="login-form__input" type="email" required></input>
         </label>
-        <label for="user-password" className="sign-form__label">Пароль
-          <input id="user-password" className="sign-form__input" type="password" minLength="6" required></input>
+        <span className="error">{errors.email || ''}</span>
+        <label for="user-password" className={`login-form__label ${errors.password ? 'login-form__label_error': ''}`}>Пароль
+          <input onChange={handleChange} value={values.password} name='password' id="user-password" className="login-form__input" type="password" minLength="6" required></input>
         </label>
+        <span className="error">{errors.password || ''}</span>
       </form>
-      <button className="sign__submit-button" type="submit" aria-label="Подтвердить вход" onClick={submitLogin}>Войти</button>
-      <div className="sign__wrapper">
-        <span className="sign__paragraph">Еще не зарегистрированы? </span>
-        <Link to="/signup" className="sign__signin-link">Регистрация</Link>
+      <button disabled={!isValid} className={`login__submit-button ${!isValid ? 'login__submit-button_invalid' :''}`} type="submit" aria-label="Подтвердить вход" onClick={handleSubmit}>Войти</button>
+      <div className="login__wrapper">
+        <span className="login__paragraph">Еще не зарегистрированы? </span>
+        <Link to="/signup" className="login__signin-link">Регистрация</Link>
       </div>
     </div>
   );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,27 +1,68 @@
 import React from 'react';
 import "./Register.css";
 import SignHeader from "../SignHeader/SignHeader";
-import {Link} from "react-router-dom";
+import * as MainApi from "../../utils/MainApi";
+import { useFormHook } from "../../utils/useFormHook";
+import {Link, useHistory} from "react-router-dom";
 
-function Register() {
+function Register({signIn}) {
+  const history = useHistory();
+  // const [name, setName] = React.useState('');
+  // const [email, setEmail] = React.useState('');
+  // const [password, setPassword] = React.useState('');
+  const { values, handleChange, errors, isValid } = useFormHook();
+
+  const [registerSuccess, setRegisterSuccess] = React.useState(false);
+
+  // function handleNameChange(e) {
+  //   setName(e.target.value);
+  // }
+  // function handleEmailChange(e) {
+  //   setEmail(e.target.value);
+  // }
+  // function handlePasswordChange(e) {
+  //   setPassword(e.target.value);
+  // }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    MainApi.register(values)
+      .then((res)=>{
+        if(res.statusCode !== 400){
+          console.log('Вы успешно зарегистрировались!');
+          setRegisterSuccess(true);
+          signIn();
+          history.push('/movies');
+        } else {
+          console.log('Что-то пошло не так! Попробуйте ещё раз.');
+        }
+      })
+      .catch((err)=> {
+        console.log(err);
+      })
+  }
+
+
   return (
     <div className="sign-container">
       <SignHeader 
         headerText="Добро пожаловать!"
       />
       <form className="sign-form">
-        <label for="user-name" className="sign-form__label">Имя
-          <input id="user-name" className="sign-form__input" type="text" minLength="2" maxLength="40" required></input>
+        <label htmlFor="user-name" className={`sign-form__label ${errors.name ? 'sign-form__label_error' :''}`}>Имя
+          <input onChange={handleChange} value={values.name} name="name" id="user-name" className="sign-form__input" type="text" minLength="2" maxLength="40" required></input>
         </label>
-        <label for="user-email" className="sign-form__label">E-mail
-          <input id="user-email" className="sign-form__input" type="email" required></input>
+        <span className="error">{errors.name || ''}</span>
+        <label htmlFor="user-email" className={`sign-form__label ${errors.name ? 'sign-form__label_error' :''}`}>E-mail
+          <input onChange={handleChange} value={values.email} name="email" id="user-email" className="sign-form__input" type="email" required></input>
         </label>
-        <label for="user-password" className="sign-form__label sign-form__error">Пароль
-          <input id="user-password" className="sign-form__input sign-form__error" type="password" minLength="6" required></input>
+        <span className="error">{errors.email || ''}</span>
+        <label htmlFor="user-password" className={`sign-form__label ${errors.name ? 'sign-form__label_error' :''}`}>Пароль
+          <input onChange={handleChange} value={values.password} name="password" id="user-password" className="sign-form__input" type="password" minLength="6" required></input>
         </label>
-        <span className="error">Что-то пошло не так...</span>
+        <span className="error">{errors.password || ''}</span>
       </form>
-      <button className="sign__submit-button" type="submit" aria-label="Подтвердить регистрацию">Зарегистрироваться</button>
+      <button onClick={handleSubmit} className={`sign__submit-button ${!isValid ? 'sign__submit-button_invalid' :''}`} type="submit" aria-label="Подтвердить регистрацию" disabled={!isValid}>Зарегистрироваться</button>
       <div className="sign__wrapper">
         <span className="sign__paragraph">Уже зарегистрированы? </span>
         <Link to="/signin" className="sign__signin-link">Войти</Link>
